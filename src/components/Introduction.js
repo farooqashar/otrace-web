290a224c3790012ac87b18a6984898760d6235d5
Ashar Farooq <ashar744@gmail.com>
2025-02-23 23:16:49 -0500

    cleanup initial introduction

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,15 +1,26 @@
 import React, { useState } from "react";
-import { TextField, Button, Container, Typography } from "@mui/material";
+import {
+  TextField,
+  Button,
+  Container,
+  Typography,
+  Snackbar,
+  Alert,
+} from "@mui/material";
 import { db } from "../firebase/firebase";
+import { useAuth } from "../firebase/auth";
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 import { Navigate } from "react-router-dom";
 
 const Introduction = ({ role }) => {
   const [formData, setFormData] = useState({
-    consumer: "",
-    operator: "",
+    dataProvider: "",
+    dataRecipient: "",
     service: "",
   });
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const { user } = useAuth();
 
   if (role !== "consumer") return <Navigate to="/login" />;
 
@@ -20,29 +31,36 @@ const Introduction = ({ role }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     await addDoc(collection(db, "attestations"), {
-      ...formData,
-      action: "Introduction",
+      action: { type: "Introduction", information: formData },
+      party: { email: user.email, role: role },
       timestamp: serverTimestamp(),
     });
-    alert("Submitted successfully!");
+    setSnackbarMessage(
+      "Attestation for an introduction has been successfully recorded!"
+    );
+    setOpenSnackbar(true);
   };
 
   return (
     <Container>
-      <Typography variant="h5">Introduction Form</Typography>
+      <Typography variant="h5">Introduction</Typography>
+      <Typography variant="h7">
+        Fill out the following form to introduce and connect two different
+        parties on behalf of yourself to utilize a traceability service.
+      </Typography>
       <form onSubmit={handleSubmit}>
         <TextField
           fullWidth
-          label="Consumer"
-          name="consumer"
+          label="Data Provider"
+          name="dataProvider"
           onChange={handleChange}
           required
           margin="normal"
         />
         <TextField
           fullWidth
-          label="Operator"
-          name="operator"
+          label="Data Recipient"
+          name="dataRecipient"
           onChange={handleChange}
           required
           margin="normal"
@@ -59,6 +77,20 @@ const Introduction = ({ role }) => {
           Submit
         </Button>
       </form>
+      {/* Snackbar Confirmation of Success */}
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={() => setOpenSnackbar(false)}
+      >
+        <Alert
+          onClose={() => setOpenSnackbar(false)}
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
     </Container>
   );
 };
