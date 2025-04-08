1282c58027abff9f571382e2a5ba5c015e701597
Ashar Farooq <ashar744@gmail.com>
2025-04-07 20:01:53 -0400

    adding to fides taxonomy

--- src/utils/fides.js
+++ src/utils/fides.js
@@ -1,30 +1,80 @@
-// Grouped Data Uses
 export const dataUsesGrouped = [
-  // Marketing
-  { label: "marketing", group: "marketing" },
-  { label: "marketing.advertising", group: "marketing" },
-  { label: "marketing.advertising.first_party", group: "marketing" },
-  { label: "marketing.communications", group: "marketing" },
-
   // Analytics
   { label: "analytics", group: "analytics" },
   { label: "analytics.reporting.ad_performance", group: "analytics" },
+  { label: "analytics.reporting.content_performance", group: "analytics" },
+  { label: "analytics.reporting.campaign_insights", group: "analytics" },
+  { label: "analytics.reporting.system", group: "analytics" },
   { label: "analytics.reporting.system.performance", group: "analytics" },
 
-  // Essential
-  { label: "essential", group: "essential" },
-  { label: "essential.fraud_detection", group: "essential" },
-  { label: "essential.service.security", group: "essential" },
-
-  // Personalize
-  { label: "personalize", group: "personalize" },
-  { label: "personalize.content", group: "personalize" },
+  // Collect
+  { label: "collect", group: "collect" },
 
   // Employment
   { label: "employment", group: "employment" },
   { label: "employment.recruitment", group: "employment" },
 
-  // AI
+  // Essential
+  { label: "essential", group: "essential" },
+  { label: "essential.fraud_detection", group: "essential" },
+  { label: "essential.legal_obligation", group: "essential" },
+  { label: "essential.service", group: "essential" },
+  { label: "essential.service.authentication", group: "essential" },
+  { label: "essential.service.notifications", group: "essential" },
+  { label: "essential.service.notifications.email", group: "essential" },
+  { label: "essential.service.notifications.sms", group: "essential" },
+  { label: "essential.service.operations", group: "essential" },
+  { label: "essential.service.operations.support", group: "essential" },
+  { label: "essential.service.operations.improve", group: "essential" },
+  { label: "essential.service.payment_processing", group: "essential" },
+  { label: "essential.service.security", group: "essential" },
+  { label: "essential.service.upgrades", group: "essential" },
+
+  // Finance
+  { label: "finance", group: "finance" },
+
+  // Functional
+  { label: "functional", group: "functional" },
+  { label: "functional.storage", group: "functional" },
+  { label: "functional.service", group: "functional" },
+  { label: "functional.service.improve", group: "functional" },
+
+  // Marketing
+  { label: "marketing", group: "marketing" },
+  { label: "marketing.advertising", group: "marketing" },
+  { label: "marketing.advertising.first_party", group: "marketing" },
+  { label: "marketing.advertising.first_party.contextual", group: "marketing" },
+  { label: "marketing.advertising.first_party.targeted", group: "marketing" },
+  { label: "marketing.advertising.frequency_capping", group: "marketing" },
+  { label: "marketing.advertising.negative_targeting", group: "marketing" },
+  { label: "marketing.advertising.profiling", group: "marketing" },
+  { label: "marketing.advertising.serving", group: "marketing" },
+  { label: "marketing.advertising.third_party", group: "marketing" },
+  { label: "marketing.advertising.third_party.targeted", group: "marketing" },
+  { label: "marketing.communications", group: "marketing" },
+  { label: "marketing.communications.email", group: "marketing" },
+  { label: "marketing.communications.sms", group: "marketing" },
+
+  // Operations
+  { label: "operations", group: "operations" },
+
+  // Personalize
+  { label: "personalize", group: "personalize" },
+  { label: "personalize.content", group: "personalize" },
+  { label: "personalize.profiling", group: "personalize" },
+  { label: "personalize.system", group: "personalize" },
+
+  // Sales
+  { label: "sales", group: "sales" },
+
+  // Third Party Sharing
+  { label: "third_party_sharing", group: "third_party_sharing" },
+  {
+    label: "third_party_sharing.legal_obligation",
+    group: "third_party_sharing",
+  },
+
+  // AI / Collection
   { label: "train_ai_system", group: "train_ai_system" },
 ];
 
@@ -36,29 +86,101 @@ export const dataCategoriesGrouped = [
 
   { label: "user", group: "user" },
   { label: "user.account", group: "user" },
+  { label: "user.account.settings", group: "user" },
+  { label: "user.account.username", group: "user" },
+
   { label: "user.authorization", group: "user" },
+  { label: "user.authorization.credentials", group: "user" },
+  { label: "user.authorization.biometric", group: "user" },
+  { label: "user.authorization.password", group: "user" },
+
   { label: "user.behavior", group: "user" },
+  { label: "user.behavior.browsing_history", group: "user" },
+  { label: "user.behavior.media_consumption", group: "user" },
+  { label: "user.behavior.purchase_history", group: "user" },
+  { label: "user.behavior.search_history", group: "user" },
+
   { label: "user.biometric", group: "user" },
+  { label: "user.biometric.fingerprint", group: "user" },
+  { label: "user.biometric.retinal", group: "user" },
+  { label: "user.biometric.voice", group: "user" },
+  { label: "user.biometric.health", group: "user" },
+
+  { label: "user.childrens", group: "user" },
+
   { label: "user.contact", group: "user" },
+  { label: "user.contact.address", group: "user" },
+  { label: "user.contact.address.city", group: "user" },
+  { label: "user.contact.address.country", group: "user" },
+  { label: "user.contact.address.postal_code", group: "user" },
+  { label: "user.contact.address.state", group: "user" },
+  { label: "user.contact.address.street", group: "user" },
+  { label: "user.contact.email", group: "user" },
+  { label: "user.contact.phone_number", group: "user" },
+  { label: "user.contact.url", group: "user" },
+  { label: "user.contact.fax_number", group: "user" },
+  { label: "user.contact.organization", group: "user" },
+
+  { label: "user.content", group: "user" },
+  { label: "user.content.private", group: "user" },
+  { label: "user.content.public", group: "user" },
+  { label: "user.content.self_image", group: "user" },
+
   { label: "user.demographic", group: "user" },
+  { label: "user.demographic.age_range", group: "user" },
+  { label: "user.demographic.date_of_birth", group: "user" },
+  { label: "user.demographic.gender", group: "user" },
+  { label: "user.demographic.language", group: "user" },
+  { label: "user.demographic.marital_status", group: "user" },
+  { label: "user.demographic.political_opinion", group: "user" },
+  { label: "user.demographic.profile", group: "user" },
+  { label: "user.demographic.race_ethnicity", group: "user" },
+  { label: "user.demographic.religious_belief", group: "user" },
+  { label: "user.demographic.sexual_orientation", group: "user" },
+
   { label: "user.device", group: "user" },
+  { label: "user.device.cookie", group: "user" },
+  { label: "user.device.cookie_id", group: "user" },
+  { label: "user.device.device_id", group: "user" },
+  { label: "user.device.ip_address", group: "user" },
+
   { label: "user.financial", group: "user" },
+  { label: "user.financial.bank_account", group: "user" },
+  { label: "user.financial.credit_card", group: "user" },
+
   { label: "user.government_id", group: "user" },
+  { label: "user.government_id.birth_certificate", group: "user" },
+  { label: "user.government_id.drivers_license_number", group: "user" },
+  { label: "user.government_id.immigration", group: "user" },
+  { label: "user.government_id.national_identification_number", group: "user" },
+  { label: "user.government_id.passport_number", group: "user" },
+  { label: "user.government_id.vehicle_registration", group: "user" },
+
   { label: "user.health_and_medical", group: "user" },
+  { label: "user.health_and_medical.genetic", group: "user" },
+  { label: "user.health_and_medical.insurance_beneficiary_id", group: "user" },
+  { label: "user.health_and_medical.record_id", group: "user" },
+
   { label: "user.location", group: "user" },
+  { label: "user.location.imprecise", group: "user" },
+  { label: "user.location.precise", group: "user" },
+
   { label: "user.name", group: "user" },
+  { label: "user.name.first", group: "user" },
+  { label: "user.name.last", group: "user" },
+
   { label: "user.payment", group: "user" },
+
   { label: "user.privacy_preferences", group: "user" },
   { label: "user.social", group: "user" },
   { label: "user.telemetry", group: "user" },
   { label: "user.unique_id", group: "user" },
+  { label: "user.unique_id.pseudonymous", group: "user" },
+  { label: "user.user_sensor", group: "user" },
   { label: "user.workplace", group: "user" },
-
-  { label: "user.contact.email", group: "user" },
-  { label: "user.device.ip_address", group: "user" },
-  { label: "user.financial.bank_account", group: "user" },
-  { label: "user.health_and_medical.record_id", group: "user" },
-  { label: "user.name.first", group: "user" },
+  { label: "user.sensor", group: "user" },
+  { label: "user.criminal_history", group: "user" },
+  { label: "user.job_title", group: "user" },
 ];
 
 export const dataSubjects = [
