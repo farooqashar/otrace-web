577137fcdd491a07ac7a2e167d05738e01fa8254
Ashar Farooq <ashar744@gmail.com>
2025-04-08 01:19:32 -0400

    fixing env variables

--- src/oktaConfig.js
+++ src/oktaConfig.js
@@ -1,8 +1,8 @@
 import { OktaAuth } from "@okta/okta-auth-js";
 
 export const oktaAuth = new OktaAuth({
-  issuer: process.env.OKTA_ISSUER,
-  clientId: process.env.OKTA_CLIENT_ID,
+  issuer: process.env.REACT_APP_OKTA_ISSUER,
+  clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
   redirectUri: window.location.origin + "/login/callback",
   scopes: ["email", "openid", "groups"],
   pkce: true,
